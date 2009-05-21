9fcfcb3d6fb1d74688d4474413f257a509f88ef2
Juho Vepsäläinen <jutuveps@jutuveps-laptop>
2009-05-21 11:54:20 +0300

    Running of test methods should work now.

--- test_runner.php
+++ test_runner.php
@@ -6,21 +6,104 @@ $possibleArgs = array(new Help(), new Loop(), );
 $programName = "TinyTest";
 $version = "0.01";
 
+$fileLoader = new FileLoader();
 $argumentChecker = new ArgumentChecker();
 $argumentChecker->checkArguments($argv);
 
+if( !$argumentChecker->foundArguments ) {
+	$testFinder = new TestFinder();
+	$testFinder->findTests();
+	$testFinder->executeTests();
+}
+
+class FileLoader {
+	public function loadFunctions( $file ) {
+		$prev_funcs = get_defined_functions();
+		$prev_funcs = $prev_funcs["user"];
+		
+		require($file);
+		
+		$cur_funcs = get_defined_functions();
+		$cur_funcs = $cur_funcs["user"];
+		
+		$added_funcs = array_diff($cur_funcs, $prev_funcs);
+		
+		return $added_funcs;
+	}
+}
+
+class TestFinder {
+	private $tests = array();
+	
+	public function findTests() {
+		$dir = dirname(__FILE__);
+		$dirHandler = opendir($dir);
+		
+		while( $fileName = readdir($dirHandler) ) {
+			if( preg_match("/_test.php$/", $fileName) ) {
+				$this->tests[] = new TestFile($fileName);
+			}
+		}
+		
+		closedir($dirHandler);
+	}
+	
+	public function executeTests() {
+		foreach( $this->tests as $test ) {
+			$test->execute();
+		}
+	}
+}
+
+class TestFile {
+	private $fileName;
+	private $tests;
+	
+	public function __construct( $fileName ) {
+		$this->fileName = $fileName;
+	}
+	
+	public function execute() {
+		$this->loadTests();
+		
+		foreach( $this->tests as $test ) {
+			$test();
+		}
+	}
+	
+	private function loadTests() {
+		global $fileLoader;
+		
+		$functions = $fileLoader->loadFunctions($this->fileName);
+		$tests = array();
+		
+		foreach( $functions as $function ) {
+			if( preg_match("/^test_/", $function) ) {
+				$tests[] = $function;
+			}
+		}
+		
+		$this->tests = $tests;
+	}
+}
+
 class ArgumentChecker {
+	public $foundArguments;
+	
 	public function checkArguments( $args ) {
 		foreach( $args as $arg ) {
 			global $possibleArgs;
 			
 			foreach( $possibleArgs as $possibleArg ) {
 				if( $possibleArg->matches($arg) ) {
+					$this->foundArguments = true;
 					$possibleArg->execute();
 					break;
 				}
 			}
 		}
+		
+		$this->foundArguments = false;
 	}
 }
 
