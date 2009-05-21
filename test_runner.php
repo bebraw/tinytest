7228100e63c31ec7c9b535dc1fbf505a40b1e177
Juho Vepsäläinen <jutuveps@jutuveps-laptop>
2009-05-21 09:14:46 +0300

    Initial commit.

---
+++ test_runner.php
@@ -0,0 +1,74 @@
+#!/usr/bin/php
+<?php
+
+$author = "Juho Vepsäläinen";
+$possibleArgs = array(new Help(), new Loop(), );
+$programName = "TinyTest";
+$version = "0.01";
+
+$argumentChecker = new ArgumentChecker();
+$argumentChecker->checkArguments($argv);
+
+class ArgumentChecker {
+	public function checkArguments( $args ) {
+		foreach( $args as $arg ) {
+			global $possibleArgs;
+			
+			foreach( $possibleArgs as $possibleArg ) {
+				if( $possibleArg->matches($arg) ) {
+					$possibleArg->execute();
+					break;
+				}
+			}
+		}
+	}
+}
+
+abstract class Argument {
+	protected $callName;
+	public $helpText;
+	
+	abstract public function execute();
+	
+	public function getCallNames() {
+		return array("-" . $this->callName[0], "--" . $this->callName);
+	}
+	
+	public function matches( $argument ) {
+		$callNames = $this->getCallNames();
+		
+		foreach( $callNames as $callName ) {
+			if( $argument == $callName ) {
+				return true;
+			}
+		}
+	}
+}
+
+class Help extends Argument {
+	protected $callName = "help";
+	public $helpText = "Shows all available arguments.";
+	
+	public function execute() {
+		global $author, $possibleArgs, $programName, $version;
+		
+		print $programName . " " . $version . " by " . $author . ".\n\n";
+		print "Possible arguments:\n";
+		foreach( $possibleArgs as $possibleArg ) {
+			$callNames = $possibleArg->getCallNames();
+			$callNameStr = str_pad($callNames[0] . ", " . $callNames[1], 16, " ", STR_PAD_LEFT);
+			
+			print $callNameStr . " - " . $possibleArg->helpText . "\n";
+		}
+	}
+}
+
+class Loop extends Argument {
+	protected $callName = "loop";
+	public $helpText = "Execute tests automatically as files are changed.";
+
+	public function execute() {
+		print 'should do the loop thingy now';
+	}
+}
+?>
